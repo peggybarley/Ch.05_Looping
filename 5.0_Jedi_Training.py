08f3baf4f7918a294ea9a8fb66a49e7563ecd4ae
barleypeg <barleypeg@urbandaleschools.com>
2019-10-10 14:41:51 -0500

    chapter five

--- 5.0_Jedi_Training.py
+++ 5.0_Jedi_Training.py
@@ -1,46 +1,47 @@
-  #Sign your name:________________
+  #Sign your name: Peggy Barley
+
+import random
+
+#1. Make the following program work.
 
 '''
- 1. Make the following program work.
-   '''  
-     print("This program takes three numbers and returns the sum.")
-     total = 0
+print()
+print("This program takes three numbers and returns the sum.")
 
-     for i in range(3):
-         x = input("Enter a number: ")
-         total = total + i
-     print("The total is:", x)
-  
-
-
-'''
-  2. Write a Python program that will use a FOR loop to print the even
-     numbers from 2 to 100, inclusive.
+total = 0
+for i in range(3):
+    num = int(input("Enter a number: "))
+    total += num
+print("The total is:", total)
 '''
 
-
-
-
+#2. Write a Python program that will use a FOR loop to print the even numbers from 2 to 100, inclusive.
 
 '''
-  3. Write a program that will use a WHILE loop to count from
-     10 down to, and including, 0. Then print the words Blast off! Remember, use
-     a WHILE loop, don't use a FOR loop.
+for i in range(2,101,2):
+    print(i)
 '''
 
 
 
+#3. Write a program that will use a WHILE loop to count from
+     #10 down to, and including, 0. Then print the words Blast off! Remember, use
+     #a WHILE loop, don't use a FOR loop.
+
+'''
+x = 10
+while x >-1:
+    print(" T minus", x)
+    x -=1
+print("Blastoff!")
+'''
 
 
 
 '''
-  4. Write a program that prints a random integer from 1 to 10 (inclusive).
+i = random.randrange(1,11)
+print(i)
 '''
-
-
-
-
-
 
 '''
   5. Write a Python program that will:
@@ -50,5 +51,12 @@
      * Print the count of the positive entries, the count of entries equal to zero,
      and the count of negative entries. Use an if, elif, else chain, not just three
      if statements.
-      
 '''
+
+total = 0
+for i in range(7):
+    num = int(input("Enter a number: "))
+    total += num
+print("The total is:", total)
+
+#Get questions from git
