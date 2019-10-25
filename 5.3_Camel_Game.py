1abd21471285b91b800da030c8f179112922002f
barleypeg <barleypeg@urbandaleschools.com>
2019-10-25 14:24:23 -0500

    chapter five

--- 5.3_Camel_Game.py
+++ 5.3_Camel_Game.py
@@ -5,6 +5,12 @@ The pseudo-code for how to code this game is in Chapter 5 of the Python Jedi boo
 
 '''
 
+print("print instructions")
+miles = 0
+thirst = 0
+camt= 0
+distance = -20
+canteens = 7
 
 
 
